f060d261d769ec6b7cb57ada0996cc1cdda01209
Heyter <ll2.lol@yandex.ru>
2017-09-19 16:12:58 +0300

    idiot.

--- lua/autorun/server/sv_proxy_blocker.lua
+++ lua/autorun/server/sv_proxy_blocker.lua
@@ -1,18 +1,47 @@
-local string.format, http_Fetch = string.format, http.Fetch
-
+local string_format, http_Fetch, player_GetHumans, string_find, string_lower = string.format, http.Fetch, player.GetHumans, string.find, string.lower
+local ipairs, type, IsValid, util_SteamIDFrom64 = ipairs, type, IsValid, util.SteamIDFrom64
 local tag = "[proxyblocker]"
 
+local function FindPlayer(yep)
+	if (type(yep) == "string") then
+		for k, v in ipairs(player_GetHumans()) do
+			if (string_find(string_lower(v:Name()), string_lower(yep))) then
+				return v
+			elseif (string_find(string_lower(v:SteamID()), string_lower(yep))) then
+				return v
+			end
+		end
+	elseif (type(yep) == "number") then
+		for k, v in ipairs(player_GetHumans()) do
+			if (string_find(v:Name(), yep)) then
+				return v
+			elseif (string_find(v:SteamID64(), yep)) then
+				return v
+			elseif (string_find(v:SteamID(), yep)) then
+				return v
+			end
+		end
+		
+		if (IsValid(player_GetHumans()[yep])) then
+			return player_GetHumans()[yep]
+		end
+	else
+		print("FindPlayer: Search yep has unexpected value type! (string expected, got "..type(yep)..")")
+	end
+end
+
 hook.Add("CheckPassword", "ProxyBlocker_Cpassword", function(sid64, ip, sPass, enteredPass, name)
-	local steamid = util.SteamIDFrom64(sid64)
+	local steamid = util_SteamIDFrom64(steamid64)
 	local url = string_format("http://proxy.mind-media.com/block/proxycheck.php?ip=%s", ip)
+	local index = FindPlayer(steamid)
 	
 	http_Fetch(url, function(info)
 		if (info == "NOT a proxy") then
-			print(string_format("%s %s don't using proxy/vpn\n", tag, name))
+			print(string_format("%s %s don't using proxy/vpn", tag, name))
 			return
 		end
 		
-		return false -- не пускаем на сервер.
+		index:Kick("Unable to validate IP address.")
 	end,
 	
 	function(err)
