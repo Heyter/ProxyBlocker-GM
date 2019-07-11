fdad914e31c1c875affd80d16dffc746d210a6e1
Heyter <ll2.lol@yandex.ru>
2019-07-11 04:21:39 +0300

    x_x

--- lua/autorun/server/sv_proxy_blocker.lua
+++ lua/autorun/server/sv_proxy_blocker.lua
@@ -1,11 +1,11 @@
 hook.Add("CheckPassword", "ProxyBlocker_Cpassword", function(sid, ip, a, b, name)
-	http.Fetch("http://proxy.mind-media.com/block/proxycheck.php?ip="..ip, function(info)		
-		if info == 'Y' then
+	http.Fetch("http://proxy.mind-media.com/block/proxycheck.php?ip="..ip, function(body)		
+		if body == 'Y' then
 			return false
 		end
 	end,
 	
 	function(err)
-		ServerLog(string.lower("%s Failed to check for player %s [%s], HTTP Error: %s\n", "[proxyblocker]", name, util.SteamIDFrom64(sid), tostring(err)))
+		ServerLog(string.format("%s Failed to check for player %s [%s], HTTP Error: %s\n", "[proxyblocker]", name, util.SteamIDFrom64(sid), tostring(err)))
 	end)
 end)
